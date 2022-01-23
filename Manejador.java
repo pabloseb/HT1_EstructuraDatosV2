7252ff1188a0cbec256d90a6aa2184e4b4cc76af
pabloseb <pabloseb.herrerba@gmail.com>
2022-01-22 18:31:27 -0600

    Se Agregaron Comentarios JavaDoc Al Documento
    Ya no se realizo algun cambio en la logica, solo se agregaron comentarios Estilo JavaDoc para las clases

--- Manejador.java
+++ Manejador.java
@@ -1,6 +1,17 @@
+/**
+ * @author Pablo Herrera 
+ * @since 22/01/2022
+ * @version 1.3
+ * 
+ * Clase que se encarga de manejar las excpeciones del programa. Verifica sobre todo que el usuario este ingresando datos validos y dentro de los requisitos
+ */
 public class Manejador {
     Manejador(){}
-
+/**
+ * Determina con un valor boolean la entrada para el menu principal
+ * @param x cadena de texto que ingresa el usuario
+ * @return verdadero si cumple con los requisitos
+ */
     public boolean val_menu_entry(String x){
         try{
             int i = Integer.parseInt(x);
@@ -15,7 +26,11 @@ public class Manejador {
             return false;
         }
     }
-
+/**
+ * Determina la validez de la entrada del usuario del menu inicial
+ * @param x cadena ingresada por el usuario
+ * @return verdadero su cumple con los requisitos
+ */
     public boolean MenuConfirmacion(String x){
         try{
             if(Integer.parseInt(x)<0 || Integer.parseInt(x) > 2){
@@ -29,7 +44,11 @@ public class Manejador {
             return false;
         }
     }
-
+/**
+ * Determina si el usuario esta ingresando una cantidad determinada para representar a los espacios de memoria
+ * @param x cadena que ingresa el usuario
+ * @return verdadero si cumple con los requisitos
+ */
     public boolean MemoryPositions(String x){
         try{
             if(Integer.parseInt(x)<0 || Integer.parseInt(x) > 6){
@@ -43,6 +62,9 @@ public class Manejador {
             return false;
         }
     }
+/**
+ * da un marco de tiempo antes de que el programa se cierre para que no se sienta brusco para el usuario
+ */
 public void ShutDownWarning(){
     try {
         Thread.sleep(3500);//muestra el mensaje por 5 segundos
--- Vista.java
+++ Vista.java
@@ -1,3 +1,10 @@
+/**
+ * @author Pablo Herrera, Pedro Marroquin & Jose Auyon
+ * @since 22/01/2022
+ * @version 2.0
+ * 
+ * Esta es la clase que lleva la logica del programa.
+ */
 public class Vista {
  public static void main(String[] args) {
      Controlador controlador = new Controlador();
